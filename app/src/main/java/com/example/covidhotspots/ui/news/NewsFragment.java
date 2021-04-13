72dd8d51eb46568c55206f506be2258d6bf9fdf2
Jphend <Jonhendry@hotmail.com>
2021-04-13 01:08:36 +0100

    Update

--- app/src/main/java/com/example/covidhotspots/ui/news/NewsFragment.java
+++ app/src/main/java/com/example/covidhotspots/ui/news/NewsFragment.java
@@ -5,19 +5,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import com.example.covidhotspots.R;
+import com.prof.rssparser.Article;
+import com.prof.rssparser.Parser;
+
+import java.util.ArrayList;
 
 public class NewsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NewsViewModel newsViewModel = ViewModelProviders.of(this).get(NewsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_news, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        newsViewModel.getText().observe(getViewLifecycleOwner(), s -> textView.setText(s));
+
+        String urlString = "https://news.google.com/rss/search?cf=all&hl=en-US&pz=1&q=COVID-19&gl=US&ceid=US:en";
+        Parser parser = new Parser();
+        parser.execute(urlString);
+        parser.onFinish(new Parser.OnTaskCompleted() {
+            @Override
+            public void onTaskCompleted(ArrayList<Article> list) {
+                
+                //what to do when the parsing is done
+                //the Array List contains all article's data. For example you can use it for your adapter.
+                System.out.println(list.toString());
+                Toast.makeText(requireActivity(),"completed", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError() {
+                Toast.makeText(requireActivity(),"error", Toast.LENGTH_SHORT).show();
+            }
+        });
         return root;
     }
 }
